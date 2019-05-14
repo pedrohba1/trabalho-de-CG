4ce138437f6237e4b54bb8886001a0ef4b08c8ec
Pedro Bufulin <pedro.almeida@ufu.br>
2019-05-14 01:04:29 -0300

    ultima esperança

--- primitives.py
+++ primitives.py
@@ -1,5 +1,6 @@
 import pygame
 import math
+import numpy
 
 # Cores
 white = (255, 255, 255)
@@ -26,7 +27,7 @@ def circulo(screen,x0, y0, r, color):
             d = d + 2 * ( x - y ) + 5
             y = y - 1
         x = x + 1
-        _circulo(x, y, x0, y0, color, overlay)
+        _circulo(x, y, x0, y0, color, screen)
     pygame.display.flip()
     
 
@@ -179,3 +180,23 @@ def quadrado(window, x1, x2, y1, y2, color):
                 window, x1 - distancia, y1, x1 - distancia, y1 - distancia, color)
             bresenham(
                 window, x1, y1 - distancia, x1 - distancia, y1 - distancia, color)
+
+
+
+
+
+def bezier(window, p1, p2, p3, color):
+
+    for t in numpy.arange(0, 1, 0.005):
+        omt = 1 - t
+        omt2 = omt * omt
+        omt3 = omt2 * omt
+        t2 = t * t
+        t3 = t2 * t
+        x = omt3 * p1[0] + ((3 * omt2) * t * p1[0]) + (3 * omt * t2 * p2[0]) + t3 * p3[0]
+        y = omt3 * p1[1] + ((3 * omt2) * t * p1[1]) + (3 * omt * t2 * p2[1]) + t3 * p3[1]
+        x = int(numpy.floor(x))
+        y = int(numpy.floor(y))
+
+        window.set_at((x, y), color)
+        pygame.display.flip()
--- window.py
+++ window.py
@@ -2,7 +2,7 @@ import pygame
 import sys
 import math
 from primitives import *
-
+import time
 
 screen_size = (800, 600)
 
@@ -16,6 +16,41 @@ window = pygame.display.set_mode(screen_size)
 pygame.display.set_caption('Trabalho computacao grafica')
 window.fill(background)
 pygame.display.flip()
+
+
+# Cor 0
+quadrado(window, 0, 50, 50, 50, black)
+# Cor 1
+quadrado(window, 50, 100, 50, 50, black)
+# Cor 2
+quadrado(window, 100, 150, 50, 50, black)
+# Cor 3
+quadrado(window, 150, 200, 50, 50, black)
+# Cor 4
+quadrado(window, 200, 250, 50, 50, black)
+# Cor 5
+quadrado(window, 250, 300, 50, 50, black)
+# Cor 6
+quadrado(window, 300, 350, 50, 50, black)
+# Cor 7
+quadrado(window, 350, 400, 50, 50, black)
+# Cor 8
+quadrado(window, 400, 450, 50, 50, black)
+# Cor 9
+quadrado(window, 450, 500, 50, 50, black)
+# Cor 10
+quadrado(window, 500, 550, 50, 50, black)
+# Cor 11
+quadrado(window, 550, 600, 50, 50, black)
+# Cor 12
+quadrado(window, 600, 650, 50, 50, black)
+# Cor 13
+quadrado(window, 650, 700, 50, 50, black)
+# Cor 14
+quadrado(window, 700, 750, 50, 50, black)
+# Cor 15
+quadrado(window, 750, 800, 50, 50, black)
+
 
 # Variaveis de controle
 clicked = False
@@ -43,17 +78,56 @@ quadrado(window, 50, 50, 150, 200, black)
 quadrado(window, 10, 40, 160, 180, black)
 # círculo
 quadrado(window, 50, 50, 200, 250, black)
+circulo(window, 25, 225, 20, black)
 # spline ou bezier
 quadrado(window, 50, 50, 250, 300, black)
+bezier(window, (10, 260), (20, 285), (45, 290), black)
 # polilinha
 quadrado(window, 50, 50, 300, 350, black)
+bresenham(window, 10, 260, 20, 280, black)
+bresenham(window, 20, 280, 30, 260, black)
+bresenham(window, 30, 260, 40, 280, black)
 
+colorAtual = black
 #
 pygame.display.flip()
 
 
-# try
-while 1:
+def muda_botao(position):
+    if (position[0] < 50 and position[0] > 0) and (position[1] > 50 and position[1] < 100):
+        print("aqui agora")
+        primitiva = 1
+        if (primitiva == 1):
+            desenha_linha(position)
+            pygame.display.update()
+
+
+def desenha_linha(position):
+    print("aqui desenahndo")
+    while 1:
+        position2 = pygame.mouse.get_pos()
+        for event in pygame.event.get():
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                print("cheguei")
+                bresenham(window, position[0], position[1],
+                          position2[0], position2[1], colorAtual)
+            if (event.type == pygame.MOUSEBUTTONUP):
+                return
+
+
+def ClickHandler(position):
+    if (position[0] < 50 and position[1] > 50):
+        muda_botao(position)
+
+
+while True:
+
+    position = pygame.mouse.get_pos()
     for event in pygame.event.get():
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            ClickHandler(position)
+
         if event.type == pygame.QUIT:
             sys.exit()
+
+    pygame.display.update()
