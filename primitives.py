0b415d875aad7128e8abb7ba01540a18cc234dcb
LeandroFontellas <50136672+LeandroFontellas@users.noreply.github.com>
2019-05-13 21:35:44 -0300

    Update primitives.py

--- primitives.py
+++ primitives.py
@@ -179,3 +179,19 @@ def quadrado(window, x1, x2, y1, y2, color):
                 window, x1 - distancia, y1, x1 - distancia, y1 - distancia, color)
             bresenham(
                 window, x1, y1 - distancia, x1 - distancia, y1 - distancia, color)
+           
+def bezier(window, p1, p2, p3, color):
+
+    for t in numpy.arange(0, 1, 0.005):
+        omt = 1 - t
+        omt2 = omt * omt
+        omt3 = omt2 * omt
+        t2 = t * t
+        t3 = t2 * t
+        x = omt3 * p1[0] + ((3 * omt2) * t * p1[0]) + (3 * omt * t2 * p2[0]) + t3 * p3[0]
+        y = omt3 * p1[1] + ((3 * omt2) * t * p1[1]) + (3 * omt * t2 * p2[1]) + t3 * p3[1]
+        x = int(numpy.floor(x))
+        y = int(numpy.floor(y))
+
+        window.set_at((x, y), color)
+        pygame.display.flip()
